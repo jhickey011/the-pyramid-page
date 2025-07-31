9995cb1976084d5426be68c2bf77a0f6bbf5d83a
jhickey011 <joseph.b.hickey@gmail.com>
2025-07-31 12:18:13 +0100

    Create getWSLFixtures.ts

---
+++ lib/getWSLFixtures.ts
@@ -0,0 +1,14 @@
+export async function getWSLFixtures() {
+  const res = await fetch(
+    "https://v3.football.api-sports.io/fixtures?league=621&season=2025",
+    {
+      headers: {
+        "x-apisports-key": process.env.API_FOOTBALL_KEY!,
+      },
+      next: { revalidate: 3600 }, // cache for 1 hour
+    }
+  );
+
+  const data = await res.json();
+  return data.response;
+}
