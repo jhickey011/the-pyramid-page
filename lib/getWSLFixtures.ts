937735a70470c2f5938c9a40626923d9d25017df
jhickey011 <joseph.b.hickey@gmail.com>
2025-07-31 13:49:19 +0100

    Update getWSLFixtures.ts

--- lib/getWSLFixtures.ts
+++ lib/getWSLFixtures.ts
@@ -1,9 +1,9 @@
 export async function getWSLFixtures() {
   const res = await fetch(
-    "https://v3.football.api-sports.io/fixtures?league=44&season=2024",
+    "https://v3.football.api-sports.io/fixtures?league=44&season=2025",
     {
       headers: {
-        "x-apisports-key": process.env.API_FOOTBALL_KEY!,
+        "x-apisports-key": process.env.API_FOOTBALL_KEY,
       },
       next: { revalidate: 3600 }, // cache for 1 hour
     }
